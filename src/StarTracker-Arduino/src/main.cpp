13fbdabf9a09d0293a2fb6a4a457e03effda0ec1
Taco <wangziqi2002vip@163.com>
2022-10-31 17:35:40 +0800

    MPU 6050 testing

--- src/StarTracker-Arduino/src/main.cpp
+++ src/StarTracker-Arduino/src/main.cpp
@@ -3,6 +3,7 @@
 // #include <SSD1306AsciiAvrI2c.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <MPU6050.h>
 #include <Servo.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
@@ -28,6 +29,9 @@ Servo servoX;
 Servo servoY;
 Adafruit_SSD1306 oled(128, 32, &Wire, OLED_RESET);
 SoftwareSerial ZigbeeSerial(softRX, softTX);
+MPU6050 mpu;
+double pitch = 0;
+double yaw = 0;
 
 const int ServoMin = 470;
 const int ServoMax = 2500;
@@ -41,6 +45,7 @@ const int ServoMax = 2500;
 3. Pointer Mode
     Point by hand and it will follow your lead*/
 const int ModeSelect = 0;
+String Mode;
 
 bool servo_driver(int Xin, int Yin) {  // Function to drive the servos
   if (Xin <= 180 and Xin >= 0) {
@@ -90,7 +95,6 @@ void oledDisplay121(String str1, String str2,
 
 void oledDisplay111(String str1, String str2,
                     String str3) {  // Function to drive the oled
-
   oled.setTextSize(1);
   oled.setCursor(0, 0);
   oled.println(str1);
@@ -99,18 +103,14 @@ void oledDisplay111(String str1, String str2,
 }
 
 void Mode1() {
-  Serial.println("Manual Mode");
   oledDisplay111("Manual Mode", "X: " + String(Xin), "Y: " + String(Yin));
   Xin = map(analogRead(pinX), 0, 1023, 0, 360);
   Yin = map(analogRead(pinY), 0, 1023, 0, 360);
   servo_driver(Xin, Yin);
-  Serial.println("Yaw: " + String(Xin) + " Pitch: " + String(Yin) +
-                 " Status: " + String(Status));
-  delay(100);  // This has a shorter delay to make the servos more responsive
+  delay(3000);  // This has a shorter delay to make the servos more responsive
 }
 
 void Mode2() {
-  Serial.println("Serial Mode");
   oledDisplay111("Serial Mode", "X: " + String(Xin), "Y: " + String(Yin));
   if (Serial.available() > 0) {
     String a = Serial.readString();
@@ -125,14 +125,11 @@ void Mode2() {
     Xin = value1.toInt();
     Yin = value2.toInt();
     Status = servo_driver(Xin, Yin);
-    Serial.println("Yaw: " + String(Xin) + " Pitch: " + String(Yin) +
-                   " Status: " + String(Status));
-    delay(2000);
+    delay(1000);
   }
 }
 
 void Mode3() {
-  Serial.println("Wireless Mode");
   oledDisplay111("Wireless Mode", "X: " + String(Xin), "Y: " + String(Yin));
   ZigbeeSerial.println("Enable Wireless Mode");
   String a;
@@ -159,10 +156,77 @@ void Mode3() {
     Xin = value1.toInt();
     Yin = value2.toInt();
     Status = servo_driver(Xin, Yin);
-    Serial.println("Yaw: " + String(Xin) + " Pitch: " + String(Yin) +
-                   " Status: " + String(Status));
-    delay(2000);
+
+    delay(1000);
   }
+}
+
+void mpuData() {
+//     {
+//     //this will update the RA degrees with sidereal time 1degree at a time
+//     //this way the object or star on the sky is tracked.
+//   if(location2>0){//for the northern hemisphere. 
+//     if( floor(LST_degrees)==LST_degrees ){ 
+//       if (LST_degrees>180){
+//         val2 = temp+(360-LST_degrees);
+//         }else{
+//         val2 = temp-LST_degrees; //use val2 = temp+LST_degrees; if you are located in the southern hemisphere.
+//         }
+//     }
+//   }else{//for the southern hemisphere.
+//    if( floor(LST_degrees)==LST_degrees ){ 
+//       if (LST_degrees>180){
+//         val2 = temp-(360-LST_degrees);
+//         }else{
+//         val2 = temp+LST_degrees; //use val2 = temp+LST_degrees; if you are located in the southern hemisphere.
+//         }
+//     }
+//   }
+//     myRTC.updateTime();
+//     LST_time();
+//     recvdata();
+//     pitch_check();
+//     yaw_check();
+//     timer = millis();
+//     Vector norm = mpu.readNormalizeGyro();
+//     //I've put the sensor with a 90 degree angle on the setup due to
+//     //cable connection problems. Because of that the data values from the mpu6050 chip are
+//     //different in this case:
+//     //roll data(X-axis) is pitch.
+//     //pitch data(Y-axis) is yaw.
+//     yaw = yaw + norm.YAxis * timeStep;
+//     pitch = pitch + norm.XAxis * timeStep;
+//     Serial.print(" Yaw = ");
+//     Serial.print(yaw);
+//     Serial.print(" Pitch = ");
+//     Serial.print(pitch);
+//     Serial.print(" LST_d = ");
+//     Serial.print(LST_degrees);
+//     Serial.print(" LST_h = ");
+//     Serial.println(LST_hours);//local sidereal time in decimal hours.
+//     delay((timeStep*1000) - (millis() - timer));//timer for the gyro.
+// }
+// void recvdata(){
+//   //This function receives data from serial as (0.00,0.00)
+//   //splits it to strings by the comma ","
+//   //than converts them to doubles 
+//     if (Serial.available() > 0){
+//         String a= Serial.readString();
+//         String value1, value2;
+//         // For loop which will separate the String in parts
+//         // and assign them the the variables we declare
+//         for (int i = 0; i < a.length(); i++) {
+//             if (a.substring(i, i+1) == ",") {
+//                 value2 = a.substring(0, i);
+//                 value1= a.substring(i+1);
+//                 break;
+//             }
+//         }
+//         val=90-value1.toFloat();
+//         val2=value2.toFloat();
+//         temp = val2;
+//     }
+// }
 }
 
 void setup() {  // Setup function
@@ -185,6 +249,10 @@ void setup() {  // Setup function
   servoY.attach(servoYPin, ServoMin, ServoMax);
   Xin = 150;
   Yin = 44;
+  while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G)) {
+  }
+  mpu.calibrateGyro();
+  mpu.setThreshold(3);
   delay(200);
   oledDisplay121("StarTracker", "Complete", "Wait for command.");
 }
@@ -196,14 +264,18 @@ void loop() {
     SW = ModeSelect;
   }
 
+  if (0 < SW < 300) {  // Manual Mode
+    Mode = "Manual";
+  } else if (300 < SW < 700) {  // Serial Mode
+    Mode = "Serial";
+  } else if (700 < SW < 1023) {  // Wireless Mode
+    Mode = "Wireless";
+  }
+
   int count = 0;
   if (count >= 50) {
     count = 0;
+    Serial.println("Yaw: " + String(Xin) + " Pitch: " + String(Yin) +
+                   " Status: " + Mode);
   }
-
-  if (0 < SW < 300) {  // Manual Mode
-
-  } else if (300 < SW < 700) {  // Serial Mode
-
-  } else if (700 < SW < 1023) {  // Wireless Mode
-  }
+}
